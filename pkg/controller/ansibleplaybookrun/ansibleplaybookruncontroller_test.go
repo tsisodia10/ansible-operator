74f0dac36efed517879f42a7ee7f9cb725badd37
tsisodia10 <tsisodia@redhat.com>
2020-08-05 20:51:46 -0400

    Add ansibleplaybookruncontroller_test.go

---
+++ pkg/controller/ansibleplaybookrun/ansibleplaybookruncontroller_test.go
@@ -0,0 +1,12 @@
+package ansibleplaybookrun_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/ansible-operator/pkg/controller/ansibleplaybookrun"
+)
+
+var _ = Describe("Ansibleplaybookruncontroller", func() {
+
+})
